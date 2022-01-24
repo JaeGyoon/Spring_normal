c561c7fb16825f5c8a1e6b398aba1cdcc71978c5
DESKTOP-LD8791U\JaeGyoon <rkdnjsxpzm@gmail.com>
2022-01-25 03:19:49 +0900

    Spring Boot 셋팅

---
+++ src/test/java/com/Sparta/HangHaeBlog/HangHaeBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sparta.HangHaeBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangHaeBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
