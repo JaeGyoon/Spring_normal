9a8a98df507bfebd101fedbd55770c2cd209bc68
DESKTOP-LD8791U\JaeGyoon <rkdnjsxpzm@gmail.com>
2022-01-25 05:39:06 +0900

    타임 스탬프 수정

--- src/main/java/com/Sparta/HangHaeBlog/Models/Timestamped.java
+++ src/main/java/com/Sparta/HangHaeBlog/Models/Timestamped.java
@@ -3,11 +3,13 @@ package com.Sparta.HangHaeBlog.Models;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@EnableJpaAuditing
 @MappedSuperclass // Entity가 자동으로 컬럼으로 인식합니다.
 @EntityListeners(AuditingEntityListener.class) // 내부 클래스가 변경 될때 마다 자동으로 업데이트합니다.
 public abstract class Timestamped
